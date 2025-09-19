9158568c184fdbf0c29355a66952eea2e063f0b2
lalkberg <leo.alkberg@gmail.com>
2025-09-19 16:18:23 +0200

    5. Else if

--- Assets/PlayerCharacter/PlayerCharacter.cs
+++ Assets/PlayerCharacter/PlayerCharacter.cs
@@ -12,16 +12,17 @@ public class PlayerCharacter : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        // Check IF we're receiving input from the D-key. If we are, move the character 1 unit to the right.
+        // Check IF we're receiving input from the D-key. If we are, move the character 1 unit/second to the right.
         if (Input.GetKey(KeyCode.D))
         {
             // Up: positive Y, Right: positive X, Down: negative X, Left: negative X
             // transform.position += new Vector3(1f, 0f, 0f);
             transform.position += new Vector3(movementSpeed * Time.deltaTime, 0f, 0f);
         }
-        else
+        // If not, check if we're receiving input from the A-key. If we are, move the character 1 unit/second to the left.
+        else if (Input.GetKey(KeyCode.A))
         {
-            Debug.Log("I am not moving.");
+            transform.position -= new Vector3(movementSpeed * Time.deltaTime, 0f, 0f);
         }
     }
 }
