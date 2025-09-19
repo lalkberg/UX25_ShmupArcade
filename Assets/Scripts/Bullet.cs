68b602b28490e335456298be09917319da7d1b6b
lalkberg <leo.alkberg@gmail.com>
2025-09-19 16:39:42 +0200

    CHALLENGE 2: Move bullet every frame

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -12,6 +12,6 @@ public class Bullet : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        transform.position += new Vector3(0, movementSpeed, 0) * Time.deltaTime;
     }
 }
