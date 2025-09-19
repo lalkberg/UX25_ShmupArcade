cc8c9b09319cead32ca79d292159c4d682fde56f
lalkberg <leo.alkberg@gmail.com>
2025-09-19 16:23:14 +0200

    6. Local variables

--- Assets/PlayerCharacter/PlayerCharacter.cs
+++ Assets/PlayerCharacter/PlayerCharacter.cs
@@ -12,17 +12,23 @@ public class PlayerCharacter : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        Vector3 input = Vector3.zero;
         // Check IF we're receiving input from the D-key. If we are, move the character 1 unit/second to the right.
         if (Input.GetKey(KeyCode.D))
         {
             // Up: positive Y, Right: positive X, Down: negative X, Left: negative X
             // transform.position += new Vector3(1f, 0f, 0f);
-            transform.position += new Vector3(movementSpeed * Time.deltaTime, 0f, 0f);
+            // transform.position += new Vector3(movementSpeed * Time.deltaTime, 0f, 0f);
+            input += new Vector3(1f, 0f);
         }
         // If not, check if we're receiving input from the A-key. If we are, move the character 1 unit/second to the left.
         else if (Input.GetKey(KeyCode.A))
         {
-            transform.position -= new Vector3(movementSpeed * Time.deltaTime, 0f, 0f);
+
+            // transform.position -= new Vector3(movementSpeed * Time.deltaTime, 0f, 0f);
+            input += new Vector3(-1f, 0f);
         }
+
+        transform.position += input * Time.deltaTime * movementSpeed;
     }
 }
