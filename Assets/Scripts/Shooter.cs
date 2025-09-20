f467c1dd2d24812c5eb20bd452a64317e3fb475f
lalkberg <leo.alkberg@gmail.com>
2025-09-20 14:30:28 +0200

    17. Made Bullets fire based on the Shoot delay

--- Assets/Scripts/Shooter.cs
+++ Assets/Scripts/Shooter.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using UnityEngine;
 
 public class Shooter : MonoBehaviour
@@ -5,10 +6,23 @@ public class Shooter : MonoBehaviour
     public GameObject bulletPrefab;
     public Transform bulletSpawnTransform;
     public Quaternion bulletDirection;
+    public float shootDelay;
+    private bool canShoot = true;
 
     public void Shoot()
     {
-                    // game object   location          rotation
-        Instantiate(bulletPrefab, bulletSpawnTransform.position, bulletDirection);
+        if (canShoot)
+        {
+                        // game object   location          rotation
+            Instantiate(bulletPrefab, bulletSpawnTransform.position, bulletDirection);
+            StartCoroutine(ShootDelayCoroutine());
+        }
+    }
+
+    private IEnumerator ShootDelayCoroutine()
+    {
+        canShoot = false;
+        yield return new WaitForSeconds(shootDelay);
+        canShoot = true;
     }
 }
